01067a5f1566717ccde3071915b477d73933680b
qahramantaha <G00375464@atu.ie>
2025-04-03 23:27:32 +0000

    add menu and homepage content with cover image

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonMenu, IonMenuButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton,  IonButtons, IonMenu, IonMenuButton],
 })
 export class HomePage {
   constructor() {}
